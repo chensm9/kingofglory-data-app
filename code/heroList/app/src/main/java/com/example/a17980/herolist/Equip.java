dfcc84fcb7da63526c26dcee2e8dadd6fdefb82a
chensm9 <2540740154@qq.com>
2018-11-17 15:07:31 +0800

    demo change

---
+++ code/heroList/app/src/main/java/com/example/a17980/herolist/Equip.java
@@ -0,0 +1,33 @@
+package com.example.a17980.herolist;
+
+import android.graphics.Bitmap;
+
+public class Equip {
+    private String name;
+    private String price;
+    private Bitmap image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+}
